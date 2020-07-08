d4666fb2bd69cdfa7234df038dce3e950281ff09
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-07-08 12:46:12 +0200

    Added color predefinitions Added onClicked() Added onHover()

--- uimanager.js
+++ uimanager.js
@@ -2,6 +2,14 @@ let numTabs = 2;
 let tabNew;
 let tabUpdate;
 
+let colFont = "#000000";
+let colLink = "#303030";
+let colPrimary = "#454545";
+let colSecondary = "#909090";
+let colPriHover = "#292929";
+let colSecHover = "#585858";
+let colTri = "#cccccc";
+
 class UIManager {
   constructor(width, height) {
     this.width = width;
@@ -9,30 +17,44 @@ class UIManager {
   }
 
   setup() {
-    tabNew = new Tab("New",0,0, this.width/numTabs, this.height/10, "grey", "black", "black", "Courier New", rem, 3, "white");
+    tabNew = new Tab("New",0,0, this.width/numTabs, this.height/10, colSecondary, colPrimary, colFont, "Courier New", rem, 1, 3, "white");
+    tabNew.setActiveState(true);
     tabNew.setup();
-    tabUpdate = new Tab("Update",0,0, this.width/numTabs, this.height/10, "grey", "black", "black", "Courier New", rem, 3, "white");
+    tabUpdate = new Tab("Update",1,0, this.width/numTabs, this.height/10, colSecondary, colPrimary, colFont, "Courier New", rem, 1, 3, "white");
+    tabUpdate.setActiveState(false);
     tabUpdate.setup();
   }
 
   draw() {
     tabNew.draw();
-    tabUpdate.translateTab(0,1);
+    tabUpdate.translateTab();
     tabUpdate.draw();
   }
 
   update(width, height) {
+    this.width = width;
+    this.height = height;
     tabNew.width = width/numTabs;
     tabNew.height = height/10;
     tabUpdate.width = width/numTabs;
     tabUpdate.height = height/10;
   }
 
-  onClicked() {
-
+  onClicked(x,y) {
+    if (x <= this.width && y <= this.height) {
+      if(tabNew.getHoverState() == true && tabNew.getActiveState() == false){
+        tabNew.onClicked();
+        tabUpdate.onClicked();
+      }
+      if(tabUpdate.getHoverState() == true && tabUpdate.getActiveState() == false){
+        tabNew.onClicked();
+        tabUpdate.onClicked();
+      }
+    }
   }
 
-  onHover() {
-
+  onHover(x,y) {
+    tabNew.onHover(x,y);
+    tabUpdate.onHover(x,y);
   }
 }
