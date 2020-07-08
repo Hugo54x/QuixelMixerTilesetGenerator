a55ce545ae38a4e72f5431418473c277b8807be8
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-07-08 12:45:15 +0200

    Added fontStroke Added hoverState and activeState Changed rect drawing Added hover and clicked functions

--- tab.js
+++ tab.js
@@ -1,5 +1,5 @@
 class Tab {
-  constructor(text, posX, posY, width, height, tabColor, bgColor, fontColor, fontType, fontSize, lineSize, lineCol) {
+  constructor(text, posX, posY, width, height, tabColor, bgColor, fontColor, fontType, fontSize, fontStroke, lineSize, lineCol) {
     this.text = text;
     this.posX = posX;
     this.posY = posY;
@@ -10,9 +10,12 @@ class Tab {
     this.fontColor = fontColor;
     this.fontType = fontType;
     this.fontSize = fontSize;
+    this.fontStroke = fontStroke;
     this.paddingTop = height/10;
     this.lineSize = lineSize/2;
     this.lineCol = lineCol;
+    this.hoverState = false;
+    this.activeState = false;
   }
 
   setup() {
@@ -46,18 +49,17 @@ class Tab {
     strokeWeight(this.lineSize+1);
     stroke(this.tabColor);
     fill(this.tabColor);
-    //line(this.lineSize*2+1, this.height+this.lineSize, this.width, this.height+this.lineSize);
-    rect(this.lineSize*2+1, this.height-1,this.width-this.lineSize*4-2 ,-(this.lineSize))
+    rect(this.lineSize*2+1, this.height-1,this.width-this.lineSize*4-2 ,-(this.lineSize));
   }
 
-  translateTab(transX, transY) {
-    translate((this.width/numTabs)*2, this.height*transX);
+  translateTab() {
+    translate((this.width/numTabs)*2*this.posX, this.height*this.posY);
   }
 
   drawText() {
     textSize(this.height/2+this.fontSize/2);
     stroke(this.fontColor);
-    strokeWeight(1);
+    strokeWeight(this.fontStroke);
     fill(this.fontColor);
     textAlign(LEFT,CENTER);
     textFont(this.fontType);
@@ -65,6 +67,8 @@ class Tab {
   }
 
   draw() {
+    this.checkState();
+    this.checkHover();
     this.drawBackground();
     this.drawTabInner();
     this.drawTabOutline();
@@ -73,10 +77,68 @@ class Tab {
   }
 
   onClicked() {
-
+    this.toggleState();
   }
 
-  onHover() {
+  onHover(x,y) {
+    if (this.width*this.posX <= x && x <= (this.width*this.posX)+this.width && 0 <= y && y <= this.height) {
+      this.setHoverState(true);
+      this.fontSize = rem+5;
+    }
+    else {
+      this.setHoverState(false);
+      this.fontSize = rem;
+    }
+  }
 
+  setHoverState(state) {
+    this.hoverState = state;
+  }
+
+  getHoverState() {
+    return this.hoverState;
+  }
+
+  checkHover() {
+    if(this.hoverState == true && this.activeState == true){
+      this.tabColor = colTri;
+    }
+    else if (this.hoverState == true && this.activeState == false) {
+      this.tabColor = colSecondary;
+    }
+    else if (this.hoverState == false && this.activeState == true) {
+      this.tabColor = colSecondary;
+    }
+    else if (this.hoverState == false && this.activeState == false) {
+      this.tabColor = colPrimary;
+    }
+  }
+
+  toggleState() {
+    if (this.activeState == true) {
+      this.setActiveState(false);
+      this.tabColor = colSecondary;
+    }
+    else {
+      this.setActiveState(true);
+      this.tabColor = colPrimary;
+    }
+  }
+
+  setActiveState(state) {
+    this.activeState = state;
+  }
+
+  getActiveState() {
+    return this.activeState;
+  }
+
+  checkState() {
+    if (this.activeState == true) {
+      this.tabColor = colSecondary;
+    }
+    else {
+      this.tabColor = colPrimary;
+    }
   }
 }
