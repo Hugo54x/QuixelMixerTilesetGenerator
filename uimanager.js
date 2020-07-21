0ecac6d2ff44c221e4bb3fb5095ec5d2646b5c7c
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-07-21 11:53:51 +0200

    Added footer Added textStyle

--- uimanager.js
+++ uimanager.js
@@ -2,6 +2,7 @@ let numTabs = 2;
 let tabNew;
 let tabUpdate;
 let content;
+let footer;
 
 let colFont = "#000000";
 let colFontNA = "#FFFFFF";
@@ -19,14 +20,15 @@ class UIManager {
   }
 
   setup() {
-    tabNew = new Tab("New",0,0, this.width/numTabs, this.height/10, colSecondary, colPrimary, colFont, "Courier New", rem, 1, 3, "white");
+    tabNew = new Tab("New",0,0, this.width/numTabs, this.height/10, colSecondary, colPrimary, colFont, "Courier New", rem, BOLD, 3, "white");
     tabNew.setActiveState(true);
     tabNew.setup();
-    tabUpdate = new Tab("Update",1,0, this.width/numTabs, this.height/10, colSecondary, colPrimary, colFontNA, "Courier New", rem, 1, 3, "white");
+    tabUpdate = new Tab("Update",1,0, this.width/numTabs, this.height/10, colSecondary, colPrimary, colFontNA, "Courier New", rem, BOLD, 3, "white");
     tabUpdate.setActiveState(false);
     tabUpdate.setup();
     content = new Content(0, this.height/10, this.width, this.height-2*(this.height/10), colSecondary);
     content.setup();
+    footer = new Footer(0, this.height-(this.height/10), this.width, this.height, colSecondary);
   }
 
   draw() {
@@ -36,6 +38,7 @@ class UIManager {
     tabUpdate.draw();
     pop();
     content.draw();
+    footer.draw();
   }
 
   update(width, height) {
@@ -43,7 +46,8 @@ class UIManager {
     this.height = height;
     tabNew.update(width, height);
     tabUpdate.update(width, height);
-    content.update(height/10, width, height-2*(height/10));
+    content.update(0, height/10, width, height-2*(height/10));
+    footer.update(0,height-2*(height/10), width, height);
   }
 
   onClicked(x,y) {
@@ -56,11 +60,15 @@ class UIManager {
         tabNew.onClicked();
         tabUpdate.onClicked();
       }
+      if (x >= 0 && y >= this.height/10 && y <= this.height-2*(this.height/10)){
+        content.onClicked(x,y);
+      }
     }
   }
 
   onHover(x,y) {
     tabNew.onHover(x,y);
     tabUpdate.onHover(x,y);
+    content.onHover(x,y);
   }
 }
