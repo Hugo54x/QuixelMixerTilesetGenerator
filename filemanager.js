eec47743bc4e123266bc7a11aa477fb49295804e
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-07-27 13:51:32 +0200

    Added file upload

--- filemanager.js
+++ filemanager.js
@@ -1,5 +1,35 @@
+let file;
+let input;
+let fileContent;
+
 class Filemanager {
   setup() {
 
+  }
+
+  upload() {
+    input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".xml";
+
+    input.onchange = e => {
+      file = e.target.files[0];
+
+      let reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+
+      reader.onload = readerEvent => {
+        fileContent = readerEvent.target.result;
+      }
+
+      reader.onloadend = end => {
+        console.log(fileContent);
+      }
+      //
+      // console.log(file.name);
+      // console.log(file.size);
+      // console.log(file.type);
+    }
+    input.click();
   }
 }
