ebcd5096701d55e681f059d6d0e35ddbc40b794f
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-08-04 12:01:35 +0200

    Added btnUpdate

--- contentUpdate.js
+++ contentUpdate.js
@@ -1,5 +1,6 @@
 let btnUpload;
 let ddVersion;
+let btnUpdate;
 
 class ContentUpdate {
   constructor(posX, posY, width, height){
@@ -10,15 +11,18 @@ class ContentUpdate {
   }
 
   setup() {
-    btnUpload = new Button(this.posX, this.posY, rem*4*scaleFactor, rem*scaleFactor, "white", "red", "Upload", NORMAL, "black", rem*0.8*scaleFactor, "Courier New", this.width*0.5-rem*4*0.5*scaleFactor, 10*scaleFactor, filemanager.upload);
+    btnUpload = new Button(this.posX, this.posY, rem*4*scaleFactor, rem*scaleFactor, "white", "red", "Upload", NORMAL, "black", rem*0.8*scaleFactor, "Courier New", this.width*0.5-rem*4*0.5*scaleFactor, rem*scaleFactor, filemanager.upload);
     btnUpload.setup();
-    ddVersion = new Dropdown(this.posX-2*rem, this.posY+4*rem*scaleFactor, 6*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width*0.5-3*rem*scaleFactor, 0);
+    ddVersion = new Dropdown(this.posX, this.posY, 6*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width*0.5-6*rem*0.5*scaleFactor, 4*rem*scaleFactor);
     ddVersion.setElements(['Version', '2020.1.3']);
     ddVersion.setup();
+    btnUpdate = new Button(this.posX, this.posY, rem*4*scaleFactor, rem*scaleFactor, "white", "red", "Update", NORMAL, "black", rem*0.8*scaleFactor, "Courier New", this.width*0.5-rem*4*0.5*scaleFactor, 6*rem*scaleFactor, filemanager.update);
+    btnUpdate.setup();
   }
 
   draw() {
     btnUpload.draw();
+    btnUpdate.draw();
     ddVersion.draw();
   }
 
@@ -27,17 +31,20 @@ class ContentUpdate {
     this.posX = posX;
     this.width = width;
     this.height = height;
-    btnUpload.update(posX, posY, 4*rem*scaleFactor, rem*scaleFactor, this.width*0.5-rem*4*0.5*scaleFactor, 10*scaleFactor, rem*0.8*scaleFactor);
-    ddVersion.update(this.posX, this.posY+4*rem*scaleFactor, this.width*0.5-3*rem*scaleFactor, 0, 6*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
+    btnUpload.update(this.posX, this.posY, 4*rem*scaleFactor, rem*scaleFactor, this.width*0.5-rem*4*0.5*scaleFactor, rem*scaleFactor, rem*0.8*scaleFactor);
+    ddVersion.update(this.posX, this.posY, this.width*0.5-6*rem*0.5*scaleFactor, 4*rem*scaleFactor, 6*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
+    btnUpdate.update(this.posX, this.posY, 4*rem*scaleFactor, rem*scaleFactor, this.width*0.5-rem*4*0.5*scaleFactor, 6*rem*scaleFactor, rem*0.8*scaleFactor);
   }
 
   onHover(x,y) {
     btnUpload.onHover(x,y);
+    btnUpdate.onHover(x,y);
     ddVersion.onHover(x,y);
   }
 
   onClicked(x,y) {
     btnUpload.onClicked(x,y);
+    btnUpdate.onClicked(x,y);
     ddVersion.onClicked(x,y);
   }
 }
