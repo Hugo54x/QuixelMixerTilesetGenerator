f89c51aed9b5e583dc583be0a6e8f89195d1be32
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-08-04 12:33:23 +0200

    Renamed drop1 to ddTextureSize Renamed drop2 to ddTiles

--- contentNew.js
+++ contentNew.js
@@ -1,5 +1,5 @@
-let drop1;
-let drop2;
+let ddTextureSize;
+let ddTiles;
 let tTextureSize;
 let tTiles;
 let tTileSize;
@@ -15,21 +15,21 @@ class ContentNew {
   }
 
   setup() {
-    drop1 = new Dropdown(this.posX, this.posY+rem*scaleFactor, 4*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width-5*rem*scaleFactor, 0);
-    drop1.setElements([256, 512, 1024, 2048, 4096]);
-    drop1.setup();
-    drop2 = new Dropdown(this.posX, this.posY+3*rem*scaleFactor, 4*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width-5*rem*scaleFactor, 0);
-    drop2.setElements([2, 4, 8, 16, 32, 64, 128]);
-    drop2.setup();
+    ddTextureSize = new Dropdown(this.posX, this.posY+rem*scaleFactor, 4*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width-5*rem*scaleFactor, 0);
+    ddTextureSize.setElements([256, 512, 1024, 2048, 4096]);
+    ddTextureSize.setup();
+    ddTiles = new Dropdown(this.posX, this.posY+3*rem*scaleFactor, 4*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width-5*rem*scaleFactor, 0);
+    ddTiles.setElements([2, 4, 8, 16, 32, 64, 128]);
+    ddTiles.setup();
     tTextureSize = new Text(this.posX, this.posY, 0, rem*scaleFactor, "Texture Size:", "Courier New", rem*scaleFactor, BOLD, colFont);
     tTiles = new Text(this.posX, this.posY, 0, 3*rem*scaleFactor, "Tiles:", "Courier New", rem*scaleFactor, BOLD, colFont);
     tTileSize = new Text(this.posX, this.posY, 0, 5*rem*scaleFactor, "", "Courier New", rem*scaleFactor, BOLD, colFont);
-    zDependents.push(drop1);
-    zDependents.push(drop2);
+    zDependents.push(ddTextureSize);
+    zDependents.push(ddTiles);
   }
 
   draw() {
-    let tilesize = drop1.getActiveElement()/drop2.getActiveElement();
+    let tilesize = ddTextureSize.getActiveElement()/ddTiles.getActiveElement();
     if (scaleFactor == 1){
       tTileSize.setText(`Tile Size:     ${tilesize}px`);
     }
@@ -57,28 +57,28 @@ class ContentNew {
     this.posX = posX;
     this.width = width;
     this.height = height;
-    drop1.update(this.posX, this.posY+rem*scaleFactor, this.width-5*rem*scaleFactor, 0, 4*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
-    drop2.update(this.posX, this.posY+3*rem*scaleFactor, this.width-5*rem*scaleFactor, 0, 4*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
+    ddTextureSize.update(this.posX, this.posY+rem*scaleFactor, this.width-5*rem*scaleFactor, 0, 4*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
+    ddTiles.update(this.posX, this.posY+3*rem*scaleFactor, this.width-5*rem*scaleFactor, 0, 4*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
     tTextureSize.update(this.posX, this.posY, 0, rem*scaleFactor, rem*scaleFactor);
     tTiles.update(this.posX, this.posY, 0, 3*rem*scaleFactor, rem*scaleFactor);
     tTileSize.update(this.posX, this.posY, 0, 5*rem*scaleFactor, rem*scaleFactor);
   }
 
   onHover(x,y) {
-    drop1.onHover(x,y);
-    drop2.onHover(x,y);
+    ddTextureSize.onHover(x,y);
+    ddTiles.onHover(x,y);
   }
 
   onClicked(x,y) {
-    if (drop1.getActiveState() == true){
-      drop1.onClicked(x,y);
+    if (ddTextureSize.getActiveState() == true){
+      ddTextureSize.onClicked(x,y);
     }
-    else if (drop2.getActiveState() == true){
-      drop2.onClicked(x,y);
+    else if (ddTiles.getActiveState() == true){
+      ddTiles.onClicked(x,y);
     }
     else {
-      drop1.onClicked(x,y);
-      drop2.onClicked(x,y);
+      ddTextureSize.onClicked(x,y);
+      ddTiles.onClicked(x,y);
     }
   }
 }
