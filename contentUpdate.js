52f854029e1a29c25d1167f43d11bbaf4995fe6b
Hugo54x <10262302+Hugo54x@users.noreply.github.com>
2020-08-12 11:23:26 +0200

    Added tFileVersion Updated ddVersion Updated btnUpdate Update tFilename

--- contentUpdate.js
+++ contentUpdate.js
@@ -2,6 +2,7 @@ let btnUpload;
 let ddVersion;
 let btnUpdate;
 let tFilename;
+let tFileVersion;
 
 class ContentUpdate {
   constructor(posX, posY, width, height){
@@ -14,12 +15,13 @@ class ContentUpdate {
   setup() {
     btnUpload = new Button(this.posX, this.posY, rem*4*scaleFactor, rem*scaleFactor, "white", "red", "Upload", NORMAL, "black", rem*0.8*scaleFactor, "Courier New", this.width*0.5-rem*4*0.5*scaleFactor, rem*scaleFactor, filemanager.upload);
     btnUpload.setup();
-    ddVersion = new Dropdown(this.posX, this.posY, 6*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width*0.5-6*rem*0.5*scaleFactor, 4*rem*scaleFactor);
+    ddVersion = new Dropdown(this.posX, this.posY, 6*rem*scaleFactor, rem*scaleFactor, "white", "Courier New", rem*scaleFactor, NORMAL, "black", colFontNA, this.width*0.5-6*rem*0.5*scaleFactor, 6*rem*scaleFactor);
     ddVersion.setElements(['Version', '2020.1.3']);
     ddVersion.setup();
-    btnUpdate = new Button(this.posX, this.posY, rem*4*scaleFactor, rem*scaleFactor, "white", "red", "Update", NORMAL, "black", rem*0.8*scaleFactor, "Courier New", this.width*0.5-rem*4*0.5*scaleFactor, 6*rem*scaleFactor, filemanager.update);
+    btnUpdate = new Button(this.posX, this.posY, rem*4*scaleFactor, rem*scaleFactor, "white", "red", "Update", NORMAL, "black", rem*0.8*scaleFactor, "Courier New", this.width*0.5-rem*4*0.5*scaleFactor, 8*rem*scaleFactor, filemanager.update);
     btnUpdate.setup();
-    tFilename = new Text(this.posX, this.posY, 0, 2.5*rem*scaleFactor, "", "Courier New", rem*scaleFactor, BOLD, colFont);
+    tFilename = new Text(this.posX, this.posY, 0, 2.5*rem*scaleFactor, "e", "Courier New", rem*scaleFactor, BOLD, colFont);
+    tFileVersion = new Text(this.posX, this.posY, 0, 4.5*rem*scaleFactor, "e", "Courier New", rem*scaleFactor, BOLD, colFont);
   }
 
   draw() {
@@ -27,6 +29,7 @@ class ContentUpdate {
     btnUpdate.draw();
     ddVersion.draw();
     tFilename.draw();
+    tFileVersion.draw();
   }
 
   update(posX, posY, width, height) {
@@ -35,7 +38,8 @@ class ContentUpdate {
     this.width = width;
     this.height = height;
     btnUpload.update(this.posX, this.posY, 4*rem*scaleFactor, rem*scaleFactor, this.width*0.5-rem*4*0.5*scaleFactor, rem*scaleFactor, rem*0.8*scaleFactor);
-    ddVersion.update(this.posX, this.posY, this.width*0.5-6*rem*0.5*scaleFactor, 4*rem*scaleFactor, 6*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
+    ddVersion.update(this.posX, this.posY, this.width*0.5-6*rem*0.5*scaleFactor, 6*rem*scaleFactor, 6*rem*scaleFactor, rem*scaleFactor, rem*scaleFactor);
+    btnUpdate.update(this.posX, this.posY, 4*rem*scaleFactor, rem*scaleFactor, this.width*0.5-rem*4*0.5*scaleFactor, 8*rem*scaleFactor, rem*0.8*scaleFactor);
     btnUpdate.update(this.posX, this.posY, 4*rem*scaleFactor, rem*scaleFactor, this.width*0.5-rem*4*0.5*scaleFactor, 6*rem*scaleFactor, rem*0.8*scaleFactor);
     tFilename.update(this.posX, this.posY, 0, 2.5*rem*scaleFactor, rem*scaleFactor);
   }
