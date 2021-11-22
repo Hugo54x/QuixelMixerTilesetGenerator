6facc3757b291f356a42fadd5cef38236ee5d661
Pierre Grimme <10262302+Hugo54x@users.noreply.github.com>
2021-11-22 20:21:24 +0100

    refactor: Adjusted default settings

--- public/electron.js
+++ public/electron.js
@@ -7,12 +7,21 @@ function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
+        height: 600,
+        maxWidth: 1600,
+        maxHeight: 1200,
+        minWidth: 400,
+        minHeight: 300,
+        backgroundColor: '#7B7B7B',
+        frame: false,
+        title: "PBR Tileset Generator",
+        webPreferences: {
+          // preload: path.join(__dirname, 'preload.js'),
+          nodeIntegration: true
+        }
   });
-
+  
+  win.setMenu(null)
   // and load the index.html of the app.
   // win.loadFile("index.html");
   win.loadURL(
@@ -29,7 +38,9 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow()
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
